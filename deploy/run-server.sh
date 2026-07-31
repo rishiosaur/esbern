2668c3baecb5dfde7561996ab981743486c0f87c
Rishi Kothari <hey@rishi.cx>
2026-07-30 18:02:01 -0700

    load server API keys from systemd credentials

--- deploy/run-server.sh
+++ deploy/run-server.sh
@@ -2,7 +2,9 @@
 set -eu
 
 ESBERN_API_TOKEN=$(/bin/cat "${CREDENTIALS_DIRECTORY:?}/api-token")
-export ESBERN_API_TOKEN
+GOOGLE_BOOKS_API_KEY=$(/bin/cat "${CREDENTIALS_DIRECTORY:?}/google-books-api-key")
+ESBERN_ANT_API_KEY=$(/bin/cat "${CREDENTIALS_DIRECTORY:?}/anthropic-api-key")
+export ESBERN_API_TOKEN GOOGLE_BOOKS_API_KEY ESBERN_ANT_API_KEY
 
 exec /opt/esbern/.venv/bin/esbern serve \
   --path /srv/esbern/reading \
